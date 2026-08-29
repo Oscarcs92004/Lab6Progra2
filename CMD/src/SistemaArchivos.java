459429c3daf210a710a2e9f78faeaa064e947a69
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-29 08:45:28 -0600

    agrego algunos metodos a la clase SistemaArchivos para el manejo de comandos mas adelante

--- CMD/src/SistemaArchivos.java
+++ CMD/src/SistemaArchivos.java
@@ -3,11 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.File;
 /**
  *
  * @author oscar
  */
 public class SistemaArchivos {
+    private File raiz;
+    private File actual;
     
+    public SistemaArchivos(){
+        raiz = new File("Consola");
+        
+        if(!raiz.exists()){
+            raiz.mkdir();
+        }
+        
+        actual = raiz;
+    }
+    
+    public File getRaiz(){
+        return raiz;
+    }
+    
+    public File getActual(){
+        return actual;
+    }
+    
+    public String getRutaActual(){
+        return actual.getAbsolutePath();
+    }
+    
+    public File buscar(String nombre){
+        return new File(actual,nombre);
+    }
+    
+    public boolean crearDir(String nombre){
+        File nuevoDir = new File(actual, nombre);
+        if(nuevoDir.exists()){
+            return false;
+        }
+        return nuevoDir.mkdir();
+    }
+    
+    public boolean cambiarAnterior(){
+        
+    }
 }
